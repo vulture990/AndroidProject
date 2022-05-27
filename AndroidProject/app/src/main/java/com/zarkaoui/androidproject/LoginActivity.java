93cfa4fea23f7f1b7cae1d658c1e6befc72dc303
Oussama1611 <oussamakrab2000@gmail.com>
2022-05-27 14:59:58 +0100

    Adding the registration function and linking the project to firebase database

--- AndroidProject/app/src/main/java/com/zarkaoui/androidproject/LoginActivity.java
+++ AndroidProject/app/src/main/java/com/zarkaoui/androidproject/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.zarkaoui.androidproject;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,14 +18,27 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
     ImageView googleBtn;
+
+    private TextView register;
+
+
+    private MaterialButton loginBtn;
+
+    private ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +46,18 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
+        //Register :
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
 
-        MaterialButton loginBtn = findViewById(R.id.loginbtn);
 
-        Intent intent = new Intent(this, UserInformationActivity.class);
+        loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(this);
+
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
 
         //Google Sign in :
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
@@ -50,18 +71,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-//                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-//                    startActivity(intent);
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
-//                }
-                startActivity(intent);
-            }
-        });
     }
 
     public void signIn(){
@@ -88,4 +97,15 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, UserInformationActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
+    }
+
+
 }
